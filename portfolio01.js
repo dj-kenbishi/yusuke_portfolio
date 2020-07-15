7db772aed8a3dccc93a9d621cd90727f6e4bc408
dj-kenbishi <68299716+dj-kenbishi@users.noreply.github.com>
2020-07-16 04:25:30 +0900

    Add files via upload

--- portfolio01.js
+++ portfolio01.js
@@ -66,3 +66,15 @@ $(function(){
         });
     });
 });
+
+// モーダルウィンドウ
+$(function(){
+    $('.js-modal-open').on('click',function(){
+        $('.js-modal').fadeIn();
+        return false;
+    });
+    $('.js-modal-close').on('click',function(){
+        $('.js-modal').fadeOut();
+        return false;
+    });
+});
